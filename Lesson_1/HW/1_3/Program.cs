9adb935c171aad3c425c8f3f70ff4a5055ab8d35
korshaa <korshaa13@gmail.com>
2023-07-03 14:10:39 +0300

    HomwWork 1_3

---
+++ Lesson_1/HW/1_3/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8 */
+
+Console.Write("Write a  number: ");
+int num = int.Parse(Console.ReadLine()!);
+
+int count = 1;
+while (count < num + 1)
+{
+    if (count % 2 == 0)
+    {
+        Console.WriteLine(count);
+        count++;
+    }
+    else
+        count++;
+}
