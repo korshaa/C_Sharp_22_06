f0c5a0ca9bd64a3bb422aac7cc3ee21c16f3977f
korshaa <korshaa13@gmail.com>
2023-07-12 10:29:50 +0300

    ClassWork 3_2

---
+++ Lesson_3/3_2/Program.cs
@@ -0,0 +1,16 @@
+﻿// 2. Напишите программу, которая принимает на вход координаты
+//    двух точек и находит расстояние между ними в 2D пространстве.
+
+
+
+double DistancePoints(int x1, int y1, int x2, int y2)
+{
+    return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+}
+
+int x_1 = int.Parse(Console.ReadLine()!);
+int y_1 = int.Parse(Console.ReadLine()!);
+int x_2 = int.Parse(Console.ReadLine()!);
+int y_2 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(DistancePoints(x_1, y_1, x_2, y_2));
