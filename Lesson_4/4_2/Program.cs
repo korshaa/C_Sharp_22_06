320d9c745309ce102c175d2112ac6156ac2f0b98
korshaa <korshaa13@gmail.com>
2023-07-13 12:39:04 +0300

    HomeWork 4_2

---
+++ Lesson_4/4_2/Program.cs
@@ -0,0 +1,15 @@
+﻿// 2. Напишите программу, которая принимает на вход
+//    число N и выдаёт произведение чисел от 1 до N.
+
+
+int ProductofNumbers(int num)
+{
+    int all_sum = 1;
+    for (int i = 1; i <= num; i++)
+        all_sum *= i;
+    return all_sum;
+}
+
+int N = int.Parse(Console.ReadLine()!);
+int result = ProductofNumbers(N);
+Console.WriteLine(result);
