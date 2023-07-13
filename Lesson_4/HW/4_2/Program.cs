3bcf9224dd547c60bc9baa5127b054801f87dc64
korshaa <korshaa13@gmail.com>
2023-07-13 21:44:52 +0300

    HomeWork 4_2

---
+++ Lesson_4/HW/4_2/Program.cs
@@ -0,0 +1,40 @@
+﻿/* Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33] */
+
+Console.WriteLine("Ввидите диапазон чисел и длину списка");
+Console.Write("Число от -> ");
+int num_1 = int.Parse(Console.ReadLine()!);
+Console.Write("Число до -> ");
+int num_2 = int.Parse(Console.ReadLine()!);
+Console.Write("Количество символов -> ");
+int num_len = int.Parse(Console.ReadLine()!);
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    
+    while (index < length)
+    {
+        collection[index] = new Random().Next(num_1, num_2);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        System.Console.Write(col[position] + " ");
+        position++;
+    }
+}
+
+int[] array = new int[num_len];
+
+
+FillArray(array);
+PrintArray(array);
