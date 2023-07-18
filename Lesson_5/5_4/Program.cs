d57bb92935e3b96c5f1dac27a2047571a77123df
korshaa <korshaa13@gmail.com>
2023-07-18 16:12:18 +0300

    ClassWor 5_4

--- Lesson_5/5_4/Program.cs
+++ Lesson_5/5_4/Program.cs
@@ -1,3 +1,57 @@
 ﻿// 4. Найдите произведение пар чисел в одномерном массиве.
 //    Парой считаем первый и последний элемент, второй и предпоследний
 //    и т.д. Результат запишите в новом массиве. 
+
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] MassNums(int size, int from, int to)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(from, to + 1);
+    }
+    return arr;
+}
+
+
+int[] NewArray(int[] arr)
+{
+    int size = arr.Length;
+    int flex_size = size / 2 + size % 2;
+    int[] new_arr = new int[flex_size];
+
+    for (int i = 0; i < size / 2; i++)
+        new_arr[i] = arr[i] * arr[size - i - 1];
+
+    if (size % 2 == 1)
+        new_arr[flex_size - 1] = arr[size / 2];
+    return new_arr;
+}
+
+Console.WriteLine("Ввидите диапазон чисел и длину списка");
+Console.Write("Количество значений в списке -> ");
+int num = int.Parse(Console.ReadLine()!);
+Console.Write("Число от -> ");
+int start = int.Parse(Console.ReadLine()!);
+Console.Write(" Число до -> ");
+int stop = int.Parse(Console.ReadLine()!);
+
+int[] mass = MassNums(num, start, stop);
+Print(mass);
+int[] newmass = NewArray(mass);
+Print(newmass);
+
+
+
