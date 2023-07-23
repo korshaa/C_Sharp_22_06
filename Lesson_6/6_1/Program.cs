6e07aa21fdff05c5517b392d39922962becae21a
korshaa <korshaa13@gmail.com>
2023-07-23 13:06:35 +0300

    ClassWork 6_1

---
+++ Lesson_6/6_1/Program.cs
@@ -0,0 +1,17 @@
+﻿// 1. Напишите программу, которая принимает на вход три числа
+//    и проверяет, может ли существовать треугольник с сторонами такой длины.
+
+
+void ExiTri(int n_1, int n_2, int n_3)
+{
+    if (n_1 < n_2 + n_3 && n_2 < n_1 + n_3 && n_3 < n_2 + n_1)
+        Console.WriteLine("The triangle exists.");
+    else
+        Console.WriteLine("The triangle does not exist.");
+}
+
+// 1 2 3, 4 2 5
+int num_1 = int.Parse(Console.ReadLine()!); 
+int num_2 = int.Parse(Console.ReadLine()!);
+int num_3 = int.Parse(Console.ReadLine()!);  
+ExiTri(num_1, num_2, num_3);
