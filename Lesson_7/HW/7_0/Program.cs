d9f6e2494ab0de5e225a3edb244a2de7ad539a47
korshaa <korshaa13@gmail.com>
2023-07-27 19:25:40 +0300

    HW_7_0

---
+++ Lesson_7/HW/7_0/Program.cs
@@ -0,0 +1,41 @@
+﻿// 1. Задайте двумерный массив размером m×n,
+// заполненный случайными вещественными числами.
+
+void Print(double[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+            Console.Write($" {arr[i, j], 6} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+double[,] MassNums(int row, int column, int from, int to)
+{
+    double[,] arr = new double[row, column];
+    Random n_new = new Random();
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = Math.Round(n_new.NextDouble() * (to - from) + from, 2);            
+    return arr;
+}
+
+Console.Write("Введите количество строк: ");
+int row_num = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов: ");
+int column_num = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите минимальное количество масива ");
+int start = int.Parse(Console.ReadLine()!);
+Console.Write("Введите максимальное количество масива ");
+int stop = int.Parse(Console.ReadLine()!);
+
+double[,] mass = MassNums(row_num, column_num, start, stop);
+
+Print(mass);
