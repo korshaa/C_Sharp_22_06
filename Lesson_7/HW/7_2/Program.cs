f8174ce5c02dd630c9cfe9fa57a41fc392a1264b
korshaa <korshaa13@gmail.com>
2023-07-27 19:33:07 +0300

    HW_7_2

---
+++ Lesson_7/HW/7_2/Program.cs
@@ -0,0 +1,56 @@
+﻿// 3. Задайте двумерный массив из целых чисел.
+//    Найдите среднее арифметическое элементов в каждом столбце.
+
+void Print(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+            Console.Write($" {arr[i, j]} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to + 1);
+    return arr;
+}
+
+void AVG(int[,] arr)
+{
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+    double res;
+
+    for (int i = 0; i < column; i++)
+    {
+        res = 0;
+        for (int j = 0; j < row; j++) res += arr[j, i];
+        Console.Write($"{Math.Round(res / row, 2)}; ");
+    }
+}
+
+Console.Write("Введите количество строк:: ");
+int row_num = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов: ");
+int column_num = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите минимальное количество масива ");
+int start = int.Parse(Console.ReadLine()!);
+Console.Write("Введите максимальное количество масива ");
+int stop = int.Parse(Console.ReadLine()!);
+
+int[,] mass = MassNums(row_num, column_num, start, stop);
+
+Print(mass);
+
+AVG(mass);
